031d2b6cb7004061006021c018d6235ee45abfeb
Muhammed Emin Beköz <m.eminbekoz19@hotmail.com>
2023-08-16 15:53:36 +0300

    login register pages included

--- app.js
+++ app.js
@@ -23,6 +23,17 @@ app.get("/about", (req, res) => {
 app.get("/blog", (req, res) => {
 	res.render("site/blog");
 });
+app.get("/contact", (req, res) => {
+	res.render("site/contact");
+});
+
+app.get("/login", (req, res) => {
+	res.render("site/login");
+});
+
+app.get("/register", (req, res) => {
+	res.render("site/register");
+});
 
 app.listen(port, hostname, () => {
 	console.log(`Example app listening on port http://${hostname}:${port} `);
