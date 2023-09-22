9cafc26b67f4533be20022c1d3adfa9efd9d8d3b
Muhammed Emin Beköz <m.eminbekoz19@hotmail.com>
2023-09-22 17:58:31 +0300

    categories page edited

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const router = express.Router();
+const Category = require("../../models/Category");
 
 router.get("/", (req, res) => {
 	res.render("admin/index");
 });
 router.get("/categories", (req, res) => {
-	res.render("admin/categories");
+	Category.find({}).then((categories) => {
+		res.render("admin/categories", { categories: categories });
+	});
+});
+
+router.post("/categories", (req, res) => {
+	Category.create(req.body);
+	res.render("categories");
 });
 
 module.exports = router;
